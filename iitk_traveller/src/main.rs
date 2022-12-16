7dbed75cc9fe04d88d4b0e87f5e0bea3aeb5bbc2
kartik-iitk <kartik21@iitk.ac.in>
2022-12-16 20:06:43 +0530

    Implemented Line by Line Reading
    The input file was read into a buffer and then read line by line.
    Trimming was implemented.

--- iitk_traveller/src/main.rs
+++ iitk_traveller/src/main.rs
@@ -1,11 +1,42 @@
+use core::panic;
 use std::env;
-use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Lines};
+use std::path::Path;
+
+// Returns an Iterator to the Reader of the lines of the file. The output is
+// wrapped in a Result to allow error matching.
+fn read_lines<P>(filename: P) -> io::Result<Lines<BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
+}
+
+fn store_input(filename: &String) {
+    let lines: Lines<BufReader<File>> =
+        read_lines(filename).expect("Error in reading file!");
+    let mut linenum = 0;
+    for l in lines {
+        let line = match l {
+            Ok(x) => x,
+            Err(error) => {
+                panic!("Error in reading line {}: {}!", linenum + 1, error);
+            }
+        };
+        linenum += 1;
+        let trim_line = line.trim();
+        println!("{}", trim_line);
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+    let filename = match args.len() {
+        2 => &args[1],
+        _ => panic!("Invalid parameters. Usage: ./interpreter <filename>"),
+    };
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-
-    println!("The contents of {} are:\n{}", filename, contents);
+    store_input(filename);
 }
