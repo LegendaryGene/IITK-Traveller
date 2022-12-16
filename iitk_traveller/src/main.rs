3de894888e661e9b5b4d9b8dc3ec490abe278027
kartik-iitk <kartik21@iitk.ac.in>
2022-12-16 17:52:56 +0530

    Added File Reading from Command Line

--- iitk_traveller/src/main.rs
+++ iitk_traveller/src/main.rs
@@ -1,3 +1,11 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("New Rust Repo!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+
+    println!("The contents of {} are:\n{}", filename, contents);
 }
