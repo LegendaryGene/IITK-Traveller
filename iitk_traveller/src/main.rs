0b4ab238e2e372577573aa7f194ccec0317b474d
kartik-iitk <kartik21@iitk.ac.in>
2022-12-17 18:41:24 +0530

    Rust Interpreter Development Completed

--- iitk_traveller/src/main.rs
+++ iitk_traveller/src/main.rs
@@ -5,11 +5,142 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader, Lines};
 use std::path::Path;
 
-fn create_memorystrip() -> Vec<i32> {
-    let mut mem: Vec<i32> = Vec::with_capacity(2048);
-    mem.push(0); // mem1
-    mem.push(0); // mem2
-    mem.push(0); // mem3
+struct TravelStat {
+    mem1: usize,
+    mem2: usize,
+    mem3: usize,
+    cond: i32,
+    curr_loc: i32,
+}
+
+impl TravelStat {
+    fn new(
+        mem1: usize,
+        mem2: usize,
+        mem3: usize,
+        cond: i32,
+        curr_loc: i32,
+    ) -> TravelStat {
+        TravelStat {
+            mem1,
+            mem2,
+            mem3,
+            cond,
+            curr_loc,
+        }
+    }
+
+    fn perform_operation(
+        &mut self,
+        operation: i32,
+        mem: &mut Vec<i32>,
+        loc: &HashMap<String, i32>,
+    ) {
+        let mut inp = "".to_string();
+
+        match operation {
+            2 => {
+                // "iit_gate_in_1"
+                io::stdin()
+                    .read_line(&mut inp)
+                    .expect("Failed to read line");
+                mem[self.mem1] = match inp.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => panic!("Invalid Input!"),
+                };
+            }
+            3 => {
+                // "iit_gate_in_2"
+                io::stdin()
+                    .read_line(&mut inp)
+                    .expect("Failed to read line");
+                mem[self.mem2] = match inp.trim().parse() {
+                    Ok(num) => num,
+                    Err(_) => panic!("Invalid Input!"),
+                };
+            }
+            4 => mem[self.mem3] = mem[self.mem1] + mem[self.mem2], // "hall_2"
+            5 => mem[self.mem3] = mem[self.mem1] * mem[self.mem2], // "hall_3"
+            6 => mem[self.mem3] = mem[self.mem1] - mem[self.mem2], // "hall_5"
+            7 => mem[self.mem3] = mem[self.mem1] / mem[self.mem2], // "hall_12"
+            8 => mem[self.mem1] = mem[self.mem3],                  // "mt_1_3"
+            9 => mem[self.mem3] = mem[self.mem1],                  // "mt_3_1"
+            10 => mem[self.mem2] = mem[self.mem3],                 // "mt_2_3"
+            11 => mem[self.mem3] = mem[self.mem2],                 // "mt_3_2"
+            12 => println!("{}", mem[self.mem1]), // "iit_gate_out_1"
+            13 => println!("{}", mem[self.mem2]), // "iit_gate_out_2"
+            14 => {
+                // "lecture_hall_gt"
+                if mem[self.mem1] > mem[self.mem2] {
+                    self.curr_loc = loc["lecture_hall_gt_t"];
+                } else {
+                    self.curr_loc = loc["lecture_hall_gt_f"];
+                }
+            }
+            17 => {
+                // "lecture_hall_lt"
+                if mem[self.mem1] < mem[self.mem2] {
+                    self.curr_loc = loc["lecture_hall_lt_t"];
+                } else {
+                    self.curr_loc = loc["lecture_hall_lt_f"];
+                }
+            }
+            20 => {
+                // "lecture_hall_eq"
+                if mem[self.mem1] >= mem[self.mem2] {
+                    self.curr_loc = loc["lecture_hall_eq_t"];
+                } else {
+                    self.curr_loc = loc["lecture_hall_eq_f"];
+                }
+            }
+            23 => mem[self.mem1] += 1, // "oat_stairs_1"
+            24 => mem[self.mem2] += 1, // "oat_stairs_2"
+            25 => self.cond += 1,      // "oat_stairs_c"
+            26 => mem[self.mem1] -= 1, // "southern_labs_1"
+            27 => mem[self.mem2] -= 1, // "southern_labs_2"
+            28 => self.cond -= 1,      // "southern_labs_c"
+            29 => mem[self.mem1] = 0,  // "hall_13_1"
+            30 => mem[self.mem2] = 0,  // "hall_13_2"
+            31 => mem[self.mem3] = 0,  // "hall_13_3"
+            32 => self.cond = 0,       // "hall_13_c"
+            33 => self.mem1 += 1,      // "rm_1"
+            34 => self.mem2 += 1,      // "rm_2"
+            35 => self.mem3 += 1,      // "rm_3"
+            36 => self.mem1 -= 1,      // "kd_1"
+            37 => self.mem2 -= 1,      // "kd_2"
+            38 => self.mem3 -= 1,      // "kd_3"
+            _ => panic!("No such operation exists!"),
+        }
+    }
+
+    fn travel(
+        &mut self,
+        mem: &mut Vec<i32>,
+        locations: &HashMap<String, i32>,
+        graph: &HashMap<i32, HashMap<i32, i32>>,
+    ) {
+        if self.curr_loc == locations["finish"] {
+            return;
+        }
+
+        if self.curr_loc != locations["start"] {
+            self.perform_operation(self.curr_loc, mem, &locations);
+        }
+
+        if !graph[&self.curr_loc].contains_key(&self.cond) {
+            panic!("Stuck in landmark number {}", self.curr_loc);
+        }
+
+        self.curr_loc = graph[&self.curr_loc][&self.cond];
+        return self.travel(mem, locations, graph);
+    }
+}
+
+fn create_memorystrip(len: i32) -> Vec<i32> {
+    let mut mem: Vec<i32> = Vec::new();
+    for _i in 0..len {
+        mem.push(0);
+    }
     return mem;
 }
 
@@ -62,131 +193,94 @@ fn store_input(filename: &String) -> Vec<Vec<String>> {
     return tokens;
 }
 
-fn create_map() -> HashMap<&str, i32> {
+fn create_map() -> HashMap<String, i32> {
     let locations = HashMap::from([
-        ("start", 0),
-        ("finish", 1),
-        ("iit_gate_in_1", 2),
-        ("iit_gate_in_2", 3),
-        ("hall_2", 4),
-        ("hall_3", 5),
-        ("hall_5", 6),
-        ("hall_12", 7),
-        ("mt_1_3", 8),
-        ("mt_3_1", 9),
-        ("mt_2_3", 10),
-        ("mt_3_2", 11),
-        ("iit_gate_out_1", 12),
-        ("iit_gate_out_2", 13),
-        ("lecture_hall_gt", 14),
-        ("lecture_hall_gt_t", 15),
-        ("lecture_hall_gt_f", 16),
-        ("lecture_hall_lt", 17),
-        ("lecture_hall_lt_t", 18),
-        ("lecture_hall_lt_f", 19),
-        ("lecture_hall_eq", 20),
-        ("lecture_hall_eq_t", 21),
-        ("lecture_hall_eq_f", 22),
-        ("oat_stairs_1", 23),
-        ("oat_stairs_2", 24),
-        ("oat_stairs_c", 25),
-        ("southern_labs_1", 26),
-        ("southern_labs_2", 27),
-        ("southern_labs_c", 28),
-        ("hall_13_1", 29),
-        ("hall_13_2", 30),
-        ("hall_13_3", 31),
-        ("hall_13_c", 32),
-        ("rm_1", 33),
-        ("rm_2", 34),
-        ("rm_3", 35),
-        ("kd_1", 36),
-        ("kd_2", 37),
-        ("kd_3", 38),
+        ("start".to_string(), 0),
+        ("finish".to_string(), 1),
+        ("iit_gate_in_1".to_string(), 2),
+        ("iit_gate_in_2".to_string(), 3),
+        ("hall_2".to_string(), 4),
+        ("hall_3".to_string(), 5),
+        ("hall_5".to_string(), 6),
+        ("hall_12".to_string(), 7),
+        ("mt_1_3".to_string(), 8),
+        ("mt_3_1".to_string(), 9),
+        ("mt_2_3".to_string(), 10),
+        ("mt_3_2".to_string(), 11),
+        ("iit_gate_out_1".to_string(), 12),
+        ("iit_gate_out_2".to_string(), 13),
+        ("lecture_hall_gt".to_string(), 14),
+        ("lecture_hall_gt_t".to_string(), 15),
+        ("lecture_hall_gt_f".to_string(), 16),
+        ("lecture_hall_lt".to_string(), 17),
+        ("lecture_hall_lt_t".to_string(), 18),
+        ("lecture_hall_lt_f".to_string(), 19),
+        ("lecture_hall_eq".to_string(), 20),
+        ("lecture_hall_eq_t".to_string(), 21),
+        ("lecture_hall_eq_f".to_string(), 22),
+        ("oat_stairs_1".to_string(), 23),
+        ("oat_stairs_2".to_string(), 24),
+        ("oat_stairs_c".to_string(), 25),
+        ("southern_labs_1".to_string(), 26),
+        ("southern_labs_2".to_string(), 27),
+        ("southern_labs_c".to_string(), 28),
+        ("hall_13_1".to_string(), 29),
+        ("hall_13_2".to_string(), 30),
+        ("hall_13_3".to_string(), 31),
+        ("hall_13_c".to_string(), 32),
+        ("rm_1".to_string(), 33),
+        ("rm_2".to_string(), 34),
+        ("rm_3".to_string(), 35),
+        ("kd_1".to_string(), 36),
+        ("kd_2".to_string(), 37),
+        ("kd_3".to_string(), 38),
     ]);
     return locations;
 }
 
-fn operations(
-    operation: &str,
-    mem: &Vec<i32>,
-    cond: &i32,
-    curr_lm: &i32,
-    loc: HashMap<&str, i32>,
-) {
-    let mut mem1 = 0;
-    let mut mem2 = 1;
-    let mut mem3 = 2;
-    let mut inp: String;
-
-    match operation {
-        "iit_gate_in_1" => {
-            io::stdin()
-                .read_line(&mut inp)
-                .expect("Failed to read line");
-            mem[mem1] = match inp.trim().parse() {
-                Ok(num) => num,
-                Err(_) => panic!("Invalid Input!"),
-            };
-        }
-        "iit_gate_in_2" => {
-            io::stdin()
-                .read_line(&mut inp)
-                .expect("Failed to read line");
-            mem[mem2] = match inp.trim().parse() {
-                Ok(num) => num,
-                Err(_) => panic!("Invalid Input!"),
-            };
-        }
-        "hall_2" => mem[mem3] = mem[mem1] + mem[mem2],
-        "hall_3" => mem[mem3] = mem[mem1] * mem[mem2],
-        "hall_5" => mem[mem3] = mem[mem1] - mem[mem2],
-        "hall_12" => mem[mem3] = mem[mem1] / mem[mem2],
-        "mt_1_3" => mem[mem1] = mem[mem3],
-        "mt_3_1" => mem[mem3] = mem[mem1],
-        "mt_2_3" => mem[mem2] = mem[mem3],
-        "mt_3_2" => mem[mem3] = mem[mem2],
-        "iit_gate_out_1" => println!("{}", mem[mem1]),
-        "iit_gate_out_2" => println!("{}", mem[mem2]),
-        "lecture_hall_gt" => {
-            if mem[mem1] > mem[mem2] {
-                *curr_lm = loc["lecture_hall_gt_t"];
-            } else {
-                *curr_lm = loc["lecture_hall_gt_f"];
-            }
-        }
-        "lecture_hall_lt" => {
-            if mem[mem1] < mem[mem2] {
-                *curr_lm = loc["lecture_hall_lt_t"];
-            } else {
-                *curr_lm = loc["lecture_hall_lt_f"];
-            }
-        }
-        "lecture_hall_eq" => {
-            if mem[mem1] >= mem[mem2] {
-                *curr_lm = loc["lecture_hall_eq_t"];
-            } else {
-                *curr_lm = loc["lecture_hall_eq_f"];
-            }
-        }
-        "oat_stairs_1" => mem[mem1] += 1,
-        "oat_stairs_2" => mem[mem2] += 1,
-        "oat_stairs_c" => *cond += 1,
-        "southern_labs_1" => mem[mem1] -= 1,
-        "southern_labs_2" => mem[mem2] -= 1,
-        "southern_labs_c" => *cond -= 1,
-        "hall_13_1" => mem[mem1] = 0,
-        "hall_13_2" => mem[mem2] = 0,
-        "hall_13_3" => mem[mem3] = 0,
-        "hall_13_c" => *cond = 0,
-        "rm_1" => mem1 += 1,
-        "rm_2" => mem2 += 1,
-        "rm_3" => mem3 += 1,
-        "kd_1" => mem1 -= 1,
-        "kd_2" => mem2 -= 1,
-        "kd_3" => mem3 -= 1,
-        _ => panic!("No such operation exists!"),
+fn build_graph(
+    tokens: &Vec<Vec<String>>,
+    locations: &HashMap<String, i32>,
+) -> HashMap<i32, HashMap<i32, i32>> {
+    let mut graph: HashMap<i32, HashMap<i32, i32>> = HashMap::new();
+    for i in 0..locations.len() {
+        graph.insert(i.try_into().unwrap(), HashMap::new());
     }
+    let length = tokens.len() / 3;
+    for linenum in 0..length {
+        let loc1 = match locations.get(&tokens[linenum][0]) {
+            Some(l) => l,
+            None => panic!(
+                "Line {}: '{}' is not a valid landmark!",
+                linenum + 1,
+                tokens[linenum][0]
+            ),
+        };
+
+        let cond_val: i32 = match tokens[linenum][1].parse() {
+            Ok(num) => num,
+            Err(_) => panic!(
+                "Line {}: Given weight is not a valid integer value!",
+                linenum + 1
+            ),
+        };
+
+        let loc2 = match locations.get(&tokens[linenum][2]) {
+            Some(l) => l,
+            None => panic!(
+                "Line {}: '{}' is not a valid landmark!",
+                linenum + 1,
+                tokens[linenum][2]
+            ),
+        };
+
+        if graph[&loc1].contains_key(&cond_val) {
+            panic!("Graph exists");
+        } else {
+            graph.get_mut(&loc1).map(|val| val.insert(cond_val, *loc2));
+        }
+    }
+    return graph;
 }
 
 fn main() {
@@ -196,6 +290,11 @@ fn main() {
         _ => panic!("Invalid parameters. Usage: ./interpreter <filename>"),
     };
 
+    let mut mem = create_memorystrip(2048);
     let tokens = store_input(filename);
     let locations = create_map();
+    let graph = build_graph(&tokens, &locations);
+    let mut traveller = TravelStat::new(0, 1, 2, 0, 0);
+
+    traveller.travel(&mut mem, &locations, &graph);
 }
